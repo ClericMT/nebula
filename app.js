20cdead2ae20677830378651f2522cece93c9afb
ClericMT <jessepeterdavis@gmail.com>
2021-01-03 10:43:02 -0800

    If a node is created while another node is active, a link will be created between the two nodes

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 //create canvas
 const canvas = document.getElementById('canvas')
 canvas.width = window.innerWidth
@@ -10,8 +9,9 @@ const inactiveColour = 'blue'
 const activeColour = 'aqua'
 const logHoursColour = 'green'
 
-//nodes
+//nodes and lines
 let n = []
+let l = []
 
 //map mouse coordinates on click
 canvas.onclick = function(e) {
@@ -22,7 +22,26 @@ canvas.onclick = function(e) {
     draw();
 }
 
-//turns node on
+function updateLines(){
+    let i = 0;
+    let j = 0;
+    let h = 0;
+    let k = 0
+    for (i = 0; i < n.length; i ++){
+        for (j = 0; j < n.length; j ++){
+            for (h = 0; h < n[i].ins.length; h ++){
+                for (k = 0; k < n[j].outs.length; k ++){
+                    if (n[i].ins[h] == n[j].id && n[j].outs[k] == n[i].id && n[i].id !== n[j].id){
+                        l.push({x1: n[i].x, y1: n[i].y, x2: n[j].x, y2: n[j].y})
+                        console.log(l)
+                    }
+                }
+            }
+        }
+    }
+}
+
+//turns node on, off
 function nodeOn(index){
     n[index].io = true;
     n[index].colour = activeColour;
@@ -33,19 +52,28 @@ function nodeOff(index){
     n[index].colour = inactiveColour;
 }
 
+//creates new node and appends ins/outs with other nodes
 function nodeCreate(x, y){
-    n.push({id: n.length, io: false, x: x, y: y, radius: initRadius, colour: inactiveColour, logger: false});
+    let i = 0
+    let nodeIns = []
+    for (i = 0; i < n.length; i++){
+        if (n[i].io == true){
+            nodeIns.push(n[i].id)
+            n[i].outs.push(n.length)
+        }
+    }
+    n.push({id: n.length, io: false, x: x, y: y, radius: initRadius, colour: inactiveColour, logger: false, ins: nodeIns, outs: []});
 }
 
 function evaluate(x, y){
     //do any nodes exist? create one : check clicked node's status
-    typeof n[0] !== 'undefined' ? check(x, y) : n.push({id: n.length, io: false, x: x, y: y, radius: initRadius, colour: inactiveColour, logger: false})
+    typeof n[0] !== 'undefined' ? check(x, y) : n.push({id: n.length, io: false, x: x, y: y, radius: initRadius, colour: inactiveColour, logger: false, ins: [], outs: []})
 
     //clicks on node
     function check(x, y) {
         let i = 0
         for (i = 0; i < n.length; i++){
-            //range
+            //determines if node has been clicked
             function clickedNode() {
                 let minX = (n[i].x - n[i].radius);
                 let maxX = (n[i].x + n[i].radius);
@@ -72,16 +100,23 @@ function evaluate(x, y){
     }
 }
 
+//draws node
 function draw() {
+    updateLines()
     const ctx = canvas.getContext('2d');
-    let i = 0
-
+    let i = 0;
+    let j = 0;
+    for (j = 0; j < l.length; j++){
+        ctx.beginPath();
+        ctx.moveTo(l[j].x1, l[j].y1);
+        ctx.lineTo(l[j].x2, l[j].y2);
+        ctx.strokeStyle = 'blue';
+        ctx.stroke();
+    }
     for (i = 0; i < n.length; i++){
         ctx.beginPath();
         ctx.arc(n[i].x, n[i].y, n[i].radius, 0, 2 * Math.PI, false);
         ctx.fillStyle = n[i].colour;
         ctx.fill()
     }
-    
-
 }
