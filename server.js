35d75230f05d8e30d807c90a1542e9869f6ec1d6
ClericMT <jessepeterdavis@gmail.com>
2021-01-25 23:44:12 -0800

    heroku

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ client.connect()
     })
     */
 
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
     
     /*
     app.delete('/nodes', (req, res) => {
